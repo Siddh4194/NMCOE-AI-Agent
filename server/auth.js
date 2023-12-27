843e40ccddd4eaf404b9c2865b1849e87299edc0
Siddhant Dhanaji Kadam <114551539+Siddh4194@users.noreply.github.com>
2023-12-28 00:31:48 +0530

    Update auth.js

--- server/auth.js
+++ server/auth.js
@@ -6,7 +6,7 @@ passport.use(new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3001/auth/google/callback",
+    callbackURL: "https://chatbot-nmce.vercel.app/auth/google/callback",
     passReqToCallback: true
   },
   function (request, accessToken, refreshToken, profile, done) {
